8b1d589e7cc1eca330058a66dce72ea1c0ab2cda
Dinis, Anatoliy | DSAID <ts-anatoliy.dinis@rakuten.com>
2021-07-05 16:50:35 +0900

    Added bootstrap and jquery and navbar basic.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get "/", to: 'homepage#home'
-=======
-  get "/", to: "homepage#home", as: "root"
->>>>>>> master
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#index'
 end
